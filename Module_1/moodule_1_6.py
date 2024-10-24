fb963ca8493d2cd7e890227d9840e20d66c5bfa4
Sergey Savin <grayen.mail@gmail.com>
2024-10-24 19:14:01 +0300

    Lesson 4

---
+++ Module_1/moodule_1_6.py
@@ -0,0 +1,7 @@
+task_count = 12
+time_count = 1.5
+course_name = 'Python'
+time_per_task = time_count / task_count
+print('Курс: ', course_name, ', всего задач: ', str(task_count),
+        ', затрачено часов: ',  str(time_count), ', среднее время выполнения ',
+        time_per_task, ' часа', sep='')
